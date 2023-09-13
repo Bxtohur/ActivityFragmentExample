8320332be18bc17d5e4c238dfb008dcd42a91ea9
Bxtohur <bitohur@gmail.com>
2023-09-13 22:39:29 +0700

    Finish feature navigation intent

--- app/src/main/java/com/bitohur/activityfragmentexample/presentation/main/MainActivity.kt
+++ app/src/main/java/com/bitohur/activityfragmentexample/presentation/main/MainActivity.kt
@@ -4,43 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.bitohur.activityfragmentexample.R
-import com.bitohur.activityfragmentexample.databinding.ActivityMain2Binding
+import com.bitohur.activityfragmentexample.databinding.ActivityMainBinding
 import com.bitohur.activityfragmentexample.presentation.fragmentone.FragmentOne
 import com.bitohur.activityfragmentexample.presentation.fragmentthree.FragmentThree
 import com.bitohur.activityfragmentexample.presentation.fragmenttwo.FragmentTwo
 
 class MainActivity : AppCompatActivity() {
 
-    private val fragmentOne: FragmentOne by lazy { FragmentOne() }
-    private val fragmentTwo: FragmentTwo by lazy { FragmentTwo() }
-    private val fragmentThree: FragmentThree by lazy { FragmentThree() }
-
-    private val binding: ActivityMain2Binding by lazy {
-        ActivityMain2Binding.inflate(layoutInflater)
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setClickButtonNavigate()
-        replaceFragment(fragmentOne)
-    }
-
-    private fun setClickButtonNavigate() {
-        binding.btnFragment1.setOnClickListener {
-            replaceFragment(fragmentOne)
-        }
-        binding.btnFragment2.setOnClickListener {
-            replaceFragment(fragmentTwo)
-        }
-        binding.btnFragment3.setOnClickListener {
-            replaceFragment(fragmentThree)
-        }
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(binding.flContainer.id, fragment)
-            .commit()
     }
 }
